4b2d89c1e2e9e3fe7be887a36bc64c911100cc21
martinbolsnes <martinbolsnes@icloud.com>
2022-03-16 11:43:56 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='flex items-center justify-center w-full h-screen'>
+        <div className='flex flex-col w-1/2 h-fit'>
+          <h1 className='font-bold text-xl mb-2'>Yosemite</h1>
+          <img
+            className='rounded-xl drop-shadow'
+            src='https://images.unsplash.com/photo-1647119126202-d13a4263d40c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
+            alt='random avatar'
+          ></img>
+          <button className='bg-zinc-800 text-white font-bold mt-2 rounded-md h-10 hover:bg-white hover:text-zinc-800 hover:border-solid hover:border-2 hover:border-zinc-800'>
+            Explore
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
